e7168cf720451986ae3899b837e320ed52c0ecdf
nady4 <nadyajerochim@gmail.com>
2025-04-29 05:15:01 -0300

    token

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import box from "./controllers/box";
 import login from "./controllers/login";
 import register from "./controllers/register";
@@ -7,20 +7,12 @@ import { verifyToken } from "./controllers/verifyToken";
 const router = Router();
 
 router.get("/", verifyToken, (req, res) => {
-  res.json({
-    message: "Welcome to the API",
-  });
+  res.status(200).json({ message: "Welcome to the API" });
 });
 
 router.post("/login", login);
 
 router.post("/register", register);
-
-router.post("/verify-token", verifyToken, (req, res) => {
-  res.json({
-    message: "Token is valid",
-  });
-});
 
 router.post("/image", box);
 
